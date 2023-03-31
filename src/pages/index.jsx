dd771d677ca01965ca8b8d75f559f1d52d92d479
Thiago <tmaylon88@gmail.com>
2023-03-31 14:05:06 -0300

    pegando os dados da api

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,33 @@
 import style from '../styles/Home.module.css'
+import {GetStaticProps} from 'next'
 
-export default function Home() {
+export async function getStaticProps(){
+  const max = 250
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${max}`)
+  const data = await res.json()
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return{
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
+
+export default function Home({pokemons}) {
   return (
-    <>
-      <h1>olá mundo</h1>
-    </>
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) =>(
+          <li key={pokemon.id}>{pokemon.name} - {pokemon.id}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
